288a74a49a2fb8b0948465d57fefe001b235aee6
HarshDonga <harshdonga99@gmail.com>
2019-08-07 09:58:27 +0530

    web integration ##2

--- app/manufacturers/views.py
+++ app/manufacturers/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import getpass
+import datetime
+import pkg_resources
+import traceback
+import sys
+sys.path.insert(0, '/home/harsh/project/med_python')
+
+from med_python.sawtooth_med.med_client import MedClient
+
+
+DEFAULT_URL = 'http://127.0.0.1:8008'
+
+def _get_keyfile(args):
+    username = args
+    home = os.path.expanduser("~")
+    key_dir = os.path.join(home, ".sawtooth", "keys")
+
+    return '{}/{}.priv'.format(key_dir, username)
 
 def index(request):
     return render(request, 'index.html')
@@ -12,8 +30,51 @@ def task(request):
         return render(request, 'transfer.html')
 
 def medtransact(request):
+    func = request.POST['func']
     medicineName = request.POST['medicineName']
-    medicineName = request.POST['medicineId']
-    medicineName = request.POST['medicineKeyContent']
-    medicineName = request.POST['medicineAllContents']
-    medicineName = request.POST['ExpiryMonths']
+    medicineID = request.POST['medicineID']
+    medicineKeyContent = request.POST['medicineKeyContent']
+    medicineAllContents = request.POST['medicineAllContents']
+    expiryMonths = request.POST['expiryMonths']
+    uname = request.POST['uname']
+    wait = request.POST['wait']
+    
+    manufactureDate = datetime.date.today()
+    expiryDate = manufactureDate + datetime.timedelta((args.expirymonths)*365/12)
+
+    keyfile = _get_keyfile(uname)
+    url = DEFAULT_URL
+    
+    if func == 'createMedicine':
+        client = MedClient(base_url = url , keyfile= keyfile)
+        if wait and wait > 0:
+            response = client.createMedicine(
+                medicineName,
+                medicineID,
+                medicineKeyContent,
+                medicineAllContents,
+                manufactureDate,
+                expiryDate,
+                stock = 0,
+                wait = wait,
+                auth_user = None,
+                auth_password = None
+            )
+        else:
+            response = client.createMedicine(
+                medicineName,
+                medicineID,
+                medicineKeyContent,
+                medicineAllContents,
+                manufactureDate,
+                expiryDate,
+                stock = 0,
+                auth_user = None,
+                auth_password = None
+            )
+        print("Response : {}".format(response))
+
+
+    return render(request, 'test.html', {'response': response})
+
+
